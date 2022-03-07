2bb77d32f01b1f33838c7243e418a16ab6a81a53
Christian Ziemski <czml1@gmx.de>
2022-03-07 06:48:54 +0100

    Another version strategy

--- README.md
+++ README.md
@@ -11,17 +11,23 @@ Have fun, but use this script on your own risk! No warranties at all.
 ## Optional versioning strategies
 
 * no versioning, one target directory
+* one subdirectory for every date (YYYY-MM-DD)
 * one subdirectory for every day of week (1..7)
 * one subdirectory per week (W01..W53) with sub-subdirectories per day of week (1..7)
 * one subdirectory per month (01..12) with sub-subdirectories per day (01..31)
 
-### Example:
+### Example with two files to save from 'SRCDIR'
 
-On 'Sunday March 6' the above four types of versioning would produce in the target directory:
+On 'Sun Mar 6 2022' the above types of versioning would produce
+in the target directory:
 
 ```
  ├── file1.txt
  ├── file2.txt
+
+ ├── 2022-03-06
+ │   ├── file1.txt
+ │   └── file2.txt
 
  ├── 7
  │   ├── file1.txt
--- simpleremotebackup.sh
+++ simpleremotebackup.sh
@@ -2,7 +2,7 @@
 #
 # Simple backup program to copy a local directory to a remote server
 # via 'sftp' protocol, using the program 'lftp'.
-# Optionally a rotating version strategy can be configured.
+# Optionally an (e.g.) rotating version strategy can be configured.
 # See VERSIONDIR below.
 #
 # When connecting to a remote server for the first time, lftp might complain or hang.
@@ -21,31 +21,48 @@ fi
 # ---------- Configuration - Start
 
 # the source directory to be saved
-SRCDIR=/home/pi/tmp/data
+SRCDIR=~/tmp/data
 
 # the target server, optionally with a port number (default: 22)
 #TGTSRV=pialu:22
 TGTSRV=pialu
 
+# user and password on the target server
+TGTUSR=pi
+# if the password is "" (empty string), the script asks for the password interactively
+# Helpful during testing...
+TGTPW=""
+
+# or the password might be right here in the script:
+#TGTPW=bad_idea
+#
+# (But there are far better solutions for configuring/storing the password!
+#  See man pages of lftp, .netrc, ssh-keygen, ssh-copy-id, ssh-add)
+
 # the directory on the target server
 TGTDIR=/home/pi/data-backup
 
-# Optional versioning strategy. There are many more possibilites.
+# ----------
+# Optional versioning strategy.
 # Choose one and comment out the others.
-# Or create other types. Reading 'man date' may help.
+# Or create other variations. Reading 'man date' may help and inspire you.
 #
 VERSIONDIR=""                  # no versioning, one target directory
+#VERSIONDIR=$(date +'%F')       # one subdirectory for every date (YYYY-MM-DD)
 #VERSIONDIR=$(date +'%u')       # one subdirectory for every day of week (1..7)
 #VERSIONDIR=$(date +'W%V/%u')   # one subdirectory per week (W01..W53) with sub-subdirectories per day of week (1..7)
 #VERSIONDIR=$(date +'M%m/%d')   # one subdirectory per month (01..12) with sub-subdirectories per day (01..31)
 #
-# Example:
-# On 'Sun Mar 6' the above four types of VERSIONDIR would produce:
-#
-#  data-backup/
+# Example with two files to save from 'SRCDIR':
+# On 'Sun Mar 6 2022' the above types of VERSIONDIR would produce
+# in the target directory 'TGTDIR':
 #
 #  ├── file1.txt
 #  ├── file2.txt
+#
+#  ├── 2022-03-06
+#  │   ├── file1.txt
+#  │   └── file2.txt
 #
 #  ├── 7
 #  │   ├── file1.txt
@@ -59,18 +76,6 @@ VERSIONDIR=""                  # no versioning, one target directory
 #  │   └── 06
 #  │       ├── file1.txt
 #  │       └── file2.txt
-
-# user and password on the target server
-TGTUSR=pi
-# if the password is "" (empty string), the script asks for the password interactively
-# Helpful during testing...
-TGTPW=""
-
-# or the password might be right here in the script:
-#TGTPW=bad_idea
-#
-# (But there are far better solutions for configuring/storing the password!
-#  See man pages of lftp, .netrc, ssh-keygen, ssh-copy-id, ssh-add)
 
 # ---------- Configuration - End
 
