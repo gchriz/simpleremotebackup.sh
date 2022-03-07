163bb1b13af647c93b6de9cfad5628bb17f3a435
Christian Ziemski <czml1@gmx.de>
2022-03-07 13:52:55 +0100

    More version strategies. Restructure code and README. New TGTCREATEBASEDIR.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # simpleremotebackup.sh
 
-A simple backup program to copy a local directory to a remote server
+A simple backup script to copy a local directory to a remote server
 via *sftp* protocol, using the program *lftp*.
 
 When connecting to a remote server for the first time, *lftp* might complain or hang.  
@@ -8,37 +8,66 @@ Please use an initial manual `ssh user@server` to complete the "known-hosts" dia
 
 Have fun, but use this script on your own risk! No warranties at all.
 
-## Optional versioning strategies
+## Optional creation of sourcedir's name as target's basedir
+
+It is possible to have the sourcedir's basename be created on the target side too.
+Or to just copy the **contents** of sourcedir to the target server.
+See the example below (TGTCREATEBASEDIR).
+
+## Optional versioning strategies (VERSIONDIR)
 
 * no versioning, one target directory
 * one subdirectory for every date (YYYY-MM-DD)
 * one subdirectory for every day of week (1..7)
+* one subdirectory for every day of week (Mon..Sun, by locale)
 * one subdirectory per week (W01..W53) with sub-subdirectories per day of week (1..7)
+* one subdirectory per week (W01..W53) with sub-subdirectories per day of week (Mon..Sun, by locale)
 * one subdirectory per month (01..12) with sub-subdirectories per day (01..31)
 
-### Example with two files to save from 'SRCDIR'
+## Examples
 
-On 'Sun Mar 6 2022' the above types of versioning would produce
-in the target directory:
+With two files to save from 'SRCDIR' '.../data'
+on 'Sun Mar 6 2022' the above types of VERSIONDIR
+would produce the following in the target directory 'TGTROOTDIR':
 
 ```
- ├── file1.txt
- ├── file2.txt
+                          with
+  TGTCREATEBASEDIR=no                 TGTCREATEBASEDIR=yes
 
- ├── 2022-03-06
- │   ├── file1.txt
- │   └── file2.txt
 
- ├── 7
- │   ├── file1.txt
- │   └── file2.txt
+  ├── file1.txt                       └── data
+  ├── file2.txt                           ├── file1.txt
+                                          └── file2.txt
 
- ├── W09
- │   └── 7
- │       ├── file1.txt
-         └── file2.txt
- ├── M03
- │   └── 06
- │       ├── file1.txt
- │       └── file2.txt
+  ├── 2022-03-06                      ├── 2022-03-06
+  │   ├── file1.txt                   │   └── data
+  │   └── file2.txt                   │       ├── file1.txt
+                                      │       └── file2.txt
+
+  ├── 7                               ├── 7
+  │   ├── file1.txt                   │   └── data
+  │   └── file2.txt                   │       ├── file1.txt
+                                      │       └── file2.txt
+
+  ├── Sun                             ├── Sun
+  │   ├── file1.txt                   │   └── data
+  │   └── file2.txt                   │       ├── file1.txt
+                                      │       └── file2.txt
+
+  ├── W09                             ├── W09
+  │   └── 7                           │   └── 7
+  │       ├── file1.txt               │       └── data
+          └── file2.txt               │           ├── file1.txt
+                                      │           └── file2.txt
+  ├── W09                             ├── W09
+  │   └── Sun                         │   └── Sun
+  │       ├── file1.txt               │       └── data
+          └── file2.txt               │           ├── file1.txt
+                                      │           └── file2.txt
+
+  ├── M03                             ├── M03
+  │   └── 06                          │   └── 06
+  │       ├── file1.txt               │       └── data
+  │       └── file2.txt               │           ├── file1.txt
+                                      │           └── file2.txt
 ```
--- simpleremotebackup.sh
+++ simpleremotebackup.sh
@@ -1,27 +1,21 @@
 #!/usr/bin/env bash
 #
-# Simple backup program to copy a local directory to a remote server
+# Simple backup script to copy a local directory to a remote server
 # via 'sftp' protocol, using the program 'lftp'.
+#
 # Optionally an (e.g.) rotating version strategy can be configured.
 # See VERSIONDIR below.
 #
-# When connecting to a remote server for the first time, lftp might complain or hang.
-# Please use an initial manual 'ssh user@server' to complete the "known-hosts" dialog.
+# When connecting to a remote sftp server for the first time,
+# lftp might complain or hang. Please use an initial manual
+# 'ssh user@target-server' to complete the "known-hosts" dialog.
 #
 # Have fun, but use this script on your own risk! No warranties at all.
-
-
-which lftp >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-    echo "Error: The program 'lftp' is required."
-    echo "Please install it with e.g.:  sudo apt install lftp"
-    exit 2
-fi
 
 # ---------- Configuration - Start
 
 # the source directory to be saved
-SRCDIR=~/tmp/data
+SRCDIR=~/important/data
 
 # the target server, optionally with a port number (default: 22)
 #TGTSRV=pialu:22
@@ -39,61 +33,59 @@ TGTPW=""
 # (But there are far better solutions for configuring/storing the password!
 #  See man pages of lftp, .netrc, ssh-keygen, ssh-copy-id, ssh-add)
 
-# the directory on the target server
-TGTDIR=/home/pi/data-backup
+# the root directory for the backups on the target server
+TGTROOTDIR=/home/pi/backups
+
+# how to construct the target directory
+#TGTCREATEBASEDIR=no       # creates .../backups/<version>/<sourcedir's contents>
+TGTCREATEBASEDIR=yes      # creates .../backups/<version>/<sourcedir_name>/<contents>
+                          #   e.g.  .../backups/<version>/data/<contents>
 
 # ----------
 # Optional versioning strategy.
-# Choose one and comment out the others.
+# Activate one and comment out the others.
 # Or create other variations. Reading 'man date' may help and inspire you.
 #
-VERSIONDIR=""                  # no versioning, one target directory
+VERSIONDIR=""                  # no versioning, simply the target directory
 #VERSIONDIR=$(date +'%F')       # one subdirectory for every date (YYYY-MM-DD)
 #VERSIONDIR=$(date +'%u')       # one subdirectory for every day of week (1..7)
+#VERSIONDIR=$(date +'%a')       # one subdirectory for every day of week (Mon..Sun, by locale)
 #VERSIONDIR=$(date +'W%V/%u')   # one subdirectory per week (W01..W53) with sub-subdirectories per day of week (1..7)
+#VERSIONDIR=$(date +'W%V/%a')   # one subdirectory per week (W01..W53) with sub-subdirectories per day of week (Mon..Sun, by locale)
 #VERSIONDIR=$(date +'M%m/%d')   # one subdirectory per month (01..12) with sub-subdirectories per day (01..31)
-#
-# Example with two files to save from 'SRCDIR':
-# On 'Sun Mar 6 2022' the above types of VERSIONDIR would produce
-# in the target directory 'TGTDIR':
-#
-#  ├── file1.txt
-#  ├── file2.txt
-#
-#  ├── 2022-03-06
-#  │   ├── file1.txt
-#  │   └── file2.txt
-#
-#  ├── 7
-#  │   ├── file1.txt
-#  │   └── file2.txt
-#
-#  ├── W09
-#  │   └── 7
-#  │       ├── file1.txt
-#          └── file2.txt
-#  ├── M03
-#  │   └── 06
-#  │       ├── file1.txt
-#  │       └── file2.txt
+
+# For some examples see the file README.md.
 
 # ---------- Configuration - End
-
-# if there is a password given, prepend a colon for later use in lftp
-[ -n "${TGTPW}" ] && TGTPW=":${TGTPW}"
 
 if [ ! -d ${SRCDIR} ] ; then
     echo "ERROR: ${SRCDIR} not found!"
     exit 1
 fi
 
+which lftp >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+    echo "Error: The program 'lftp' is required."
+    echo "Please install it with e.g.:  sudo apt install lftp"
+    exit 2
+fi
+
+# if there is a password given, prepend a colon for later use in lftp
+[ -n "${TGTPW}" ] && TGTPW=":${TGTPW}"
 
 LFTP_CFG="set net:timeout 5; set net:max-retries 1;"
-LFTP_CMD="mirror --reverse --verbose"
+LFTP_CMD="mirror --reverse --verbose=2"
 LFTP_SRV=sftp://${TGTUSR}${TGTPW}@${TGTSRV}
-LFTP_DIR=${TGTDIR}/${VERSIONDIR-}
+LFTP_DIR=${TGTROOTDIR}
+[ -n "${VERSIONDIR}" ] && LFTP_DIR="${LFTP_DIR}/${VERSIONDIR}"
+[ "${TGTCREATEBASEDIR}" == "yes" ] && LFTP_DIR=${LFTP_DIR}/$(basename $SRCDIR)
 
 echo "Copying '${SRCDIR}' to '${TGTUSR}@${TGTSRV}:${LFTP_DIR}'"
 
 lftp -e "${LFTP_CFG}; lcd ${SRCDIR} && ${LFTP_CMD} -O ${LFTP_DIR} ; quit" ${LFTP_SRV}
+
+# The possible final message from lftp, "To be removed: ..."
+# denotes some older/other data in the target directory LFTP_DIR.
+# That could be automatically deleted by lftp with the additional option '--delete'.
+# BUT BE VERY CAREFULL or all other parallel backups are gone when done wrong!
 
